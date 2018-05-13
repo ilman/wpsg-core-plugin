bd344bf1a9ce7840e9ebc158662c222353eeac3b
Ilman Maulana <ilman.maulana@gmail.com>
2018-05-14 00:15:57 +0700

    Update sg_post_list shortcode

--- framework/shortcodes/sg_sc_post.php
+++ framework/shortcodes/sg_sc_post.php
@@ -90,123 +90,185 @@ function sc_sg_post_meta($attr=array()){
 add_shortcode('sg_post_meta', 'sc_sg_post_meta');
 
 
+function _sg_validate_wp_query($attr){
+	$query = array();
+	$wp_key = array('author','author_name','author__in','author__not_in','cat','category_name','category__and','category__in','category__not_in','tag','tag_id','tag__and','tag__in','tag__not_in','tag_slug__and','tag_slug__in','tax_query','p','name','title','page_id','pagename','post_parent','post_parent__in','post_parent__not_in','post__in','post__not_in','post_name__in','has_password','post_password','post_type','post_status','comment_count','nopaging','posts_per_page','posts_per_archive_page','offset','paged','page','ignore_sticky_posts','order','orderby','year','monthnum','w','day','hour','minute','second','m','date_query','meta_key','meta_value','meta_value_num','meta_compare','meta_query');
 
-function sc_sg_post_list($attr=array(), $content=null){
-	global $post;
+	$custom_key = array('order_by','no_paging','post_id','post_name','post_slug','post_parent_id','category','cat_id','order_by_meta','post_include','post_exclude','post_slug_include','post_parent_include','post_parent_exclude','cat_include','cat_exclude','tag_include','tag_exclude','tag_slug_include','limit','skip');
+	
+	$array_key = array('p','name','page_id','author','author_name','cat','category_name','tag','tag_id','post_type','post_status','order','orderby','post_id','post_name','post_slug','category');
 
-	$temp_post = $post;
+	// set default value
+	if(!isset($attr['post_type'])){ $attr['post_type'] = 'post'; }
+	if(!isset($attr['limit'])){ $attr['limit'] = get_option('posts_per_page', 10); }
+	if(!isset($attr['order_by']) && !isset($attr['order_by_meta'])){ $attr['order_by'] = 'ID'; }
+	if(!isset($attr['order'])){ $attr['order'] = 'ASC'; }
+	if(!isset($attr['ignore_sticky_posts'])){ $attr['ignore_sticky_posts'] = true; }
 
-	// extract the attributes into variables
-	extract(shortcode_atts(array(
-		'post_type' => 'post',
-		'limit'	=> get_option('posts_per_page', 10),
-		'order_by' => 'ID',
-		'order' => 'asc',
-		'list' => 'default',
-		'class' => '',
-		'item_rel' => '',
-		'style' => '',
-		'col_width' => 4,
-		'is_single' => false,
-		'skip_query' => false,
-		'category' => '',
-		'tag' => '',
-	), $attr));
+	// map common key
+	foreach($attr as $key=>$value){
+		// if key is not in wp_key or not in our custom_key skip it
+		if(!in_array($key, $wp_key) && !in_array($key, $custom_key)){
+			continue;
+		}
 
-	if(isset($attr['post_type'])){ unset($attr['post_type']); }
-	if(isset($attr['limit'])){ unset($attr['limit']); }
-	if(isset($attr['offset'])){ unset($attr['offset']); }
-	if(isset($attr['order'])){ unset($attr['order']); }
-	if(isset($attr['order_by'])){ unset($attr['order_by']); }
-	if(isset($attr['list'])){ unset($attr['list']); }
-	if(isset($attr['class'])){ unset($attr['class']); }
-	if(isset($attr['item_rel'])){ unset($attr['item_rel']); }
-	if(isset($attr['style'])){ unset($attr['style']); }
-	if(isset($attr['col_width'])){ unset($attr['col_width']); }
-	if(isset($attr['category'])){ unset($attr['category']); }
-	if(isset($attr['tag'])){ unset($attr['tag']); }
+		// change value to array for supporting parameters
+		if(in_array($key, $array_key)){
+			if(!is_array($value) && $value){
+				$value = trim(str_replace(' ','',$value));
+			}
+		}
+		elseif(strpos($key, '__in')!==false || strpos($key, '__not_in')!==false || strpos($key, '_include')!==false || strpos($key, '_exclude')!==false || strpos($key, '__and')!==false){
+			if(!is_array($value) && $value){
+				$value = trim(str_replace(' ','',$value));
+			}
+		}
 
-	// map attr
-	$param_args = array();
-	$param_meta = array();
-	$arr_compare = array('=', '!=', '>', '>=', '<', '<=', 'LIKE', 'NOT LIKE', 'IN', 'NOT IN', 'BETWEEN', 'NOT BETWEEN', 'EXISTS', 'NOT EXISTS');
+		// map our custom key to wp query parameters
+		if($key=='post_name' || $key=='post_slug'){
+			$query['post_name__in'] = explode(',', $value);
+		}
+		
+		elseif($key=='post_id' || $key=='post_include'){
+			$query['post__in'] = explode(',', $value);
+		}
+		elseif($key=='post_exclude'){
+			$query['post__not_in'] = explode(',', $value);
+		}
+		
+		elseif($key=='post_parent_id' || $key=='post_parent_include'){
+			$query['post_parent__in'] = explode(',', $value);
+		}
+		elseif($key=='post_parent_exclude'){
+			$query['post_parent__not_in'] = explode(',', $value);
+		}
 
-	if($attr){
-		foreach($attr as $key=>$val){
-			if(strpos($key, 'where_meta_')===0){
+		elseif($key=='cat_id' || $key=='cat_include'){
+			$query['category__in'] = explode(',', $value);
+		}
+		elseif($key=='cat_exclude'){
+			$query['category__not_in'] = explode(',', $value);
+		}
 
-				$arr_val = explode(' ', $val, 3);
-				$arr_val_0 = strtoupper(trim(SG_Util::val($arr_val, 0)));
-				$arr_val_1 = strtoupper(trim(SG_Util::val($arr_val, 1)));
-				if(in_array($arr_val_0, $arr_compare)){
-					$val = SG_Util::val($arr_val, 1).' '.SG_Util::val($arr_val, 2);
-					$compare = $arr_val_0;
-				}
-				elseif($arr_val_0 == 'NOT' && in_array($arr_val_1, $arr_compare)){
-					$val = SG_Util::val($arr_val, 2);
-					$compare = $arr_val_0.' '.$arr_val_1;
-				}
-				else{
-					$compare = '=';
-				}
+		elseif($key=='tag_id' || $key=='tag_include'){
+			$query['tag__in'] = explode(',', $value);
+		}
+		elseif($key=='tag_exclude'){
+			$query['tag__not_in'] = explode(',', $value);
+		}
 
-				$param_meta[] = array(
-					'key' => str_replace('where_meta_', '', $key),
-					'value' => $val,
-					'compare' => $compare
+		elseif($key=='post_name_include' || $key=='post_slug_include'){
+			$query['post_name__in'] = explode(',', $value);
+		}
+
+		elseif($key=='tag_slug_include'){
+			$query['tag_slug__in'] = explode(',', $value);
+		}
+
+		elseif($key=='no_paging'){
+			$query['nopaging'] = $value;
+		}
+		elseif($key=='limit'){
+			$query['posts_per_page'] = $value;
+		}
+		elseif($key=='skip'){
+			$query['offset'] = $value;
+		}
+		elseif($key=='order_by'){
+			$query['orderby'] = $value;
+		}
+		elseif($key=='order_by_meta'){
+			$query['orderby'] = 'meta_value_num';
+			$query['meta_key'] = $value;
+		}
+		else{
+			$query[$key] = $value;
+		}
+
+		unset($attr[$key]);
+	}
+
+	// map meta_query, example: [sg_post_list where_meta_$key=">= $value"]
+
+	$meta_query = array();
+	$array_compare = array('=', '!=', '>', '>=', '<', '<=', 'LIKE', 'NOT LIKE', 'IN', 'NOT IN', 'BETWEEN', 'NOT BETWEEN', 'EXISTS', 'NOT EXISTS');
+
+	foreach($attr as $key=>$val){
+		if(strpos($key, 'where_meta_')===0){
+
+			$array_val = explode(' ', $val, 2);
+			
+			if(in_array($array_val[0], $array_compare)){
+				$param_meta = array(
+					'value' => $array_val[1],
+					'compare' => $array_val[0],
 				);
 			}
 			else{
-				$decode_val = json_decode($val, true);
-				$param_args[$key] = ($decode_val) ? $decode_val : $val;
+				$param_meta = array(
+					'value' => $array_val[0],
+					'compare' => '=',
+				);
 			}
+
+			$param_meta['key'] = trim(str_replace('where_meta_', '', $key));
+			$meta_query[] = $param_meta;
+
+			unset($attr[$key]);
 		}
 	}
 
-	$args = array(
-		'post_type' => $post_type, 
-		'posts_per_page' => $limit,
-		'orderby' => $order_by,
-		'order' => $order,
-	);
-
-	if(isset($category) && $category){
-		$args['category_name'] = $category;
+	if(count($meta_query)){
+		$query['meta_query'] = $meta_query;
 	}
 
-	if(isset($tag) && $tag){
-		$args['tag'] = $tag;
-	}
+	// map tax_query, example: [sg_post_list where_taxonomy_month_destination="january"]
 
-	if($param_meta){
-		$args['meta_query'] = $param_meta;
-	}
+	$tax_query = array();
 
-	$args = array_merge($args, $param_args);
+	foreach($attr as $key=>$val){
+		if(strpos($key, 'where_taxonomy_')===0){
 
-	$sg_post = null;
-	if(!isset($skip_query) || !$skip_query){
-		$sg_post = new WP_Query($args);
-	}
+			$tax_query[] = array(
+				'taxonomy' => trim(str_replace('where_taxonomy_', '', $key)),
+				'field' => 'slug',
+				'terms' => trim($val),
+			);
 
-	if($is_single){
-		if($content){
-			do_shortcode($content);
+			unset($attr[$key]);
 		}
-		else{
-			$output = '<h2>'.$sg_post->get_the_title().'</h2>';
-			$output .= $sg_post->get_the_content();
-		}
+	}
+
+	if(count($tax_query)){
+		$query['tax_query'] = $tax_query;
+	}
+
+	return $query;
+}
+
+
+function sc_sg_post_list($attr=array(), $content=null){
+	// extract the attributes into variables
+	extract(shortcode_atts(array(
+		'list' => 'default',
+		'class' => '',
+		'style' => '',
+		'col_width' => 4,
+	), $attr));
+	
+	global $post;
+	$temp_post = $post;
+
+	$args = _sg_validate_wp_query($attr);
+	$sg_post = new WP_Query($args);
+
+	if(file_exists(sg_view_path('framework/templates/list-'.$list.'.php'))){
+		ob_start();
+			include(sg_view_path('framework/templates/list-'.$list.'.php'));
+		$output = ob_get_clean();
 	}
 	else{
-		if(file_exists(sg_view_path('framework/templates/list-'.$list.'.php'))){
-			ob_start();
-				include(sg_view_path('framework/templates/list-'.$list.'.php'));
-			$output = ob_get_clean();
-		}
-		else{
-			$output = sg_view_path('framework/templates/list-'.$list.'.php').' file not found';
-		}
+		$output = sg_view_path('framework/templates/list-'.$list.'.php').' file not found';
 	}
 
 	$post = $temp_post;
@@ -257,7 +319,7 @@ function sc_sg_post_single($attr=array(), $content=null){
 		while ( $sg_post->have_posts() ){
 			$sg_post->the_post();
 
-			$output = do_shortcode(get_the_content());
+			$output = do_shortcode(get_the_content().sg_action_post_link(get_the_ID()));
 		}		
 	}
 
